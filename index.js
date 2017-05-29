c22a119ab19f9a24717f1823f23e6104f7d99f47
andrunix <andrunix@gmail.com>
2017-05-29 07:31:22 -0400

    Parsed the URL and got the passcode

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+
+
 var request = require('request');
+const cheerio = require('cheerio');
 
 // http://was1t7:12000/PassCode/BAMPIPassCode/passcode.htm
 
 request('http://was1t7:12000/PassCode/BAMPIPassCode/passcode.htm', function(error, response, body) {
-  console.log('error: ', error);
-  console.log('statusCode: ', response && response.statusCode);
-  console.log('body: ', body);
+  // console.log('error: ', error);
+  // console.log('statusCode: ', response && response.statusCode);
+  // console.log('body: ', body);
+
+  // now parse that body and get the BAMPI key
+  const $ = cheerio.load(body);
+
+  var text = $('b').first().parent().text();
+  var pc = text.indexOf('PassCode');
+  var beg = text.indexOf('[', pc) + 1;
+  var end = text.indexOf(']', pc);
+  var code = text.substring(beg, end);
+  
+
+
+  console.log(code);
 });
