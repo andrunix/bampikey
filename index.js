28c3a30d3c6357453310090127205367b79540f5
Andrew Pierce <andrunix@gmail.com>
2017-05-29 15:18:31 -0400

    Modularizing...

--- index.js
+++ index.js
@@ -1,32 +1,6 @@
+const memberservicekeys = require('memberservicekeys');
 
 
-const request = require('request');
-const cheerio = require('cheerio');
-
-// http://was1t7:12000/PassCode/BAMPIPassCode/passcode.htm
-
-var getBampiKey = function getBampiKey() {
-
-  return new Promise(function(resolve, reject) {
-    request('http://was1t7:12000/PassCode/BAMPIPassCode/passcode.htm', function(error, response, body) {
-      // console.log('error: ', error);
-      // console.log('statusCode: ', response && response.statusCode);
-      // console.log('body: ', body);
-
-      // now parse that body and get the BAMPI key
-      const $ = cheerio.load(body);
-
-      const text = $('b').first().parent().text();
-      const pc = text.indexOf('PassCode');
-      const beg = text.indexOf('[', pc) + 1;
-      const end = text.indexOf(']', pc);
-      const code = text.substring(beg, end);
-
-      resolve(code);
-    });
-  });
-}
-
-getBampiKey().then(function(result) {
+memberservicekeys.getBampiKey().then(function(result) {
   console.log(result);
 });
