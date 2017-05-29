905076f478a06cfdff4fc1a1b071e60e02312996
andrunix <andrunix@gmail.com>
2017-05-29 06:53:48 -0400

    Added Spring MVC Url and made the initial call.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var request = require('request');
 
-request('http://google.com', function(error, response, body) {
+// http://was1t7:12000/PassCode/BAMPIPassCode/passcode.htm
+
+request('http://was1t7:12000/PassCode/BAMPIPassCode/passcode.htm', function(error, response, body) {
   console.log('error: ', error);
   console.log('statusCode: ', response && response.statusCode);
   console.log('body: ', body);
